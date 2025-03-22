4556fcd82d7d6c8707766b4c8c825d874d12d2a2
Rohit <rohit_ghosh@epam.com>
2025-03-22 11:46:07 +0530

    update 2

--- src/sections/Header/Profile.jsx
+++ src/sections/Header/Profile.jsx
@@ -8,11 +8,11 @@ class Profile extends React.Component {
       <img src={this.props.content.profile} className='rounded-4xl' alt="Profile Picture" />
 
       <p>
-      "Hi, I'm Rohit Ghosh, a Software Developer at EPAM Systems with a strong foundation in Computer Science and Engineering. I have experience in full-stack development, machine learning, and building scalable, user-centric applications using technologies like ReactJS, Django, and Node.js. My journey includes impactful internships at HP India and Salesforce, alongside certifications in AI, GenAI, and full-stack development. As a former Project Coordinator for Enactus KIIT, I've also demonstrated leadership by managing cross-functional teams and delivering successful initiatives. I'm passionate about solving real-world problems through innovative and collaborative solutions.
+        "Hi, I'm Rohit Ghosh, a Software Developer at EPAM Systems with a strong foundation in Computer Science and Engineering. I have experience in full-stack development, machine learning, and building scalable, user-centric applications using technologies like ReactJS, Django, and Node.js. My journey includes impactful internships at HP India and Salesforce, alongside certifications in AI, GenAI, and full-stack development. As a former Project Coordinator for Enactus KIIT, I've also demonstrated leadership by managing cross-functional teams and delivering successful initiatives. I'm passionate about solving real-world problems through innovative and collaborative solutions.
       </p>
 
       <div className='flex flex-col justify-evenly'>
-
+        
       </div>
 
     </section>
